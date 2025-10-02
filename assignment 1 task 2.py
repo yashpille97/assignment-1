df63765d89af7110c2cbe08fbf1b5587e863093c
yashpille97 <yashpille2007@gmail.com>
2025-10-02 21:18:12 +0530

    Add files via upload

---
+++ assignment 1 task 2.py
@@ -0,0 +1,8 @@
+#  Takes a user's first name and last name as input.
+first_name=input("Enter your first name: ")
+last_name=input("Enter your last name: ")
+# Concatenating names with a space
+full_name=first_name + " " + last_name
+#  Prints a personalized greeting message using the full name.
+print("Hello," ,full_name)
+print("Welcome To the Python Program")
