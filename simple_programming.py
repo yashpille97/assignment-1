ab0a399f35ab06afec3118a338e741c3dcebeb01
yashpille97 <yashpille2007@gmail.com>
2025-10-02 21:15:58 +0530

    Add files via upload

---
+++ simple_programming.py
@@ -0,0 +1,38 @@
+# Takes two numbers as input from the user:
+num1=float(input("Enter the first number: "))
+num2=float(input("Enter the second number: "))
+# Performs the basic mathematical operations on these two numbers:
+addition=num1+num2
+subtraction=num1-num2
+multiplication=num1*num2
+
+division=num1/num2
+
+
+# Displays the results of each operation on the screen.
+print("\nResults:")
+print("ADDITION" ,addition)
+print("SUBTRACTION" ,subtraction)
+print("MULTIPLICATION" ,multiplication)
+print("DIVISION" ,division)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
